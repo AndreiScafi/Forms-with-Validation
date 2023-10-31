16baf187daaecb4c3c52770f4c430314c3878e3b
Andrei Scafi <andreiscafi@hotmail.com>
2023-10-31 08:04:59 -0300

    Making the heading changes with Javascript.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.querySelector('.container');
 const signUpBtn = document.querySelector('.signup-btn');
 const signInBtn = document.querySelector('.signin-btn');
+const headingSpan2 = document.querySelector('.heading-span-2');
 
 signUpBtn.addEventListener('click', () => {
     container.classList.add('change');
+    headingSpan2.textContent = "Up";
 });
 
 signInBtn.addEventListener('click', () => {
     container.classList.remove('change');
+    headingSpan2.textContent = "In";
 })
